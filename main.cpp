b16945bbdc7f80b63ad3d77dea3ec6fb11cb4266
maitre <guer.quentin@gmail.com>
2023-01-16 11:46:07 +0100

    pressqe finni

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include "BasicEncrypt.h"
 #include "Encrypt.h"
 #include "Cesar.h"
+#include "Caesar2.h"
+#include "Vigenere.h"
 
 int main()
 {
@@ -12,13 +14,28 @@ int main()
 	test.encode();
 
 	std::cout << test.getCypher() << std::endl;
-	*/
+	
 
 	Cesar test;
 	test.setCypher("ohv vdqjorwv orqjv ghv ylrorqv gh o'dxwrpqh eohvvhqw prq frhxu g'yqh odqjxhxu prqrwrqh.");
 	test.decalage(3);
 	test.decode();
-	
+
+	Caesar2 test;
+	test.setCypher("ohv vdqjorwv orqjv ghv ylrorqv gh o'dxwrpqh eohvvhqw prq frhxu g'yqh odqjxhxu prqrwrqh.");
+	test.decalage(34);
+	test.decode();*/
+	std::vector<int> cle;
+	cle.push_back(13);
+	cle.push_back(2);
+	cle.push_back(5);
+	cle.push_back(9);
+	cle.push_back(17);
+	Vigenere test;
+	test.setCypher("aaaaa");
+	test.setCle(cle);
+	test.decode();
+
 
 	std::cout << "Le message " << test.getCypher() << " dechiffre donne : " << test.getPlain() << std::endl;
 
